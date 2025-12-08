db5e2bfcd04693a4ff21eb3c0764fa6019dfde37
calidion <calidion@gmail.com>
2025-12-08 22:53:42 +0800

    removed uselss package in .toml and .lock and update test_build.sh

--- test_build.sh
+++ test_build.sh
@@ -4,6 +4,8 @@
 
 set -e
 
+sudo apt install gcc 
+
 # Enable Rust coverage instrumentation
 export RUSTFLAGS="-C instrument-coverage -Awarnings"
 export LLVM_PROFILE_FILE=".output/cargo-test-%p-%m.profraw"
@@ -17,16 +19,28 @@ PROFRAW_FILES=$(find . -name ".output/*.profraw")
 
 # Generate coverage report if grcov is installed
 rm -rf coverages || true
-if command -v grcov &> /dev/null; then
-    echo "Generating coverage report with grcov..."
-    cargo llvm-cov nextest --html --output-dir coverages
-    xdg-open coverages/html/index.html
-else
-    echo "grcov not found. Skipping coverage report generation."
-    echo "Install grcov for coverage reports: cargo install grcov"
+if ! [command -v grcov &> /dev/null]; then
+    echo "Installing grcov"
+    cargo install grcov
 fi
+
+if ! [command -v llvm-cov &> /dev/null]; then
+    echo "Installing llvm-cov"
+    cargo install cargo-llvm-cov
+fi
+
+if ! [command -v nextest &> /dev/null]; then
+    echo "Installing cargo-nextest"
+    cargo install cargo-nextest
+fi
+
+echo "Generating coverage report with grcov..."
+cargo llvm-cov nextest --html --output-dir coverages
 
 # Clean up .profraw files
 rm -rf .output/
-
 echo "Test and coverage script completed."
+
+# Show result in browser
+
+xdg-open coverages/html/index.html
